562da3ece644c84a59fadb76fb147f252f846722
Eric Song <ericksong91@gmail.com>
2022-07-01 21:31:25 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+const deleteMain = document.querySelector('main');
+deleteMain.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "ERIC is the champion"
+document.body.append(newHeader);
+
+
+
+
+
+
+
